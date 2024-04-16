9ff360341fc03a3112fe82f4ce4fec28f1db6c30
Malachite40 <dylancronkhite1@gmail.com>
2024-04-16 00:25:51 -0700

    es link

--- app/.eslintrc.cjs
+++ app/.eslintrc.cjs
@@ -27,6 +27,7 @@ const config = {
     "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-redundant-type-constituents": "off",
+    "@typescript-eslint/no-unnecessary-type-assertion": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
